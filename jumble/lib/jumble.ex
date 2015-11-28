b3103394f931895eb7111509455f1ea6fd4d77ee
Reid Paape <wrpaape@gmail.com>
2015-11-27 18:14:33 -0600

    creating jumble solver mix

---
+++ jumble/lib/jumble.ex
@@ -0,0 +1,25 @@
+defmodule Jumble do
+  @dict "/usr/share/dict/words"
+   |> File.read!
+   |> String.split
+   |> Enum.group_by(fn(string) ->
+     string
+     |> String.codepoints
+     |> Enum.sort
+   end)
+
+  def main([solution | jumbles]) do
+    jumbles
+    |> Enum.map(fn(jumble) ->
+      jumble
+      |> String.to_char_list
+      |> Enum.reduce("\\b", fn(char, acc) ->
+        [char]
+      end)
+    end)
+  end
+
+  def solve_jumble(jumble) do
+    
+  end
+end
